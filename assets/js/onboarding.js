a85b90828b182f134a6e2fd2046fcdc0df803faa
Minoo <minoo.hadi2904@gmail.com>
2023-01-02 16:30:57 +0330

    feat: link onboarding page and home page

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -2,10 +2,14 @@ const show1 = document.querySelector(".show1");
 const show2 = document.querySelector(".show2");
 
 setTimeout(function () {
-    show1.style.display = "none";
-    show2.style.display = "block"
-}, 2000)
+  show1.style.display = "none";
+  show2.style.display = "block";
+}, 2000);
 
 show2.addEventListener("click", () => {
-    window.location.href = "getstart.html"
-})
+  if (localStorage.getItem("email")) {
+    window.location.href = "home.html";
+  } else {
+    window.location.href = "getstart.html";
+  }
+});
