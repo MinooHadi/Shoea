5a1b24d01a6f6c4df3ec2970bb1e19abaa256d3e
Minoo <minoo.hadi2904@gmail.com>
2023-01-02 16:19:32 +0330

    feat: link login page and home page

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,10 @@
 const signinBtn = document.querySelector("button");
+const email = document.querySelector("input[type=email]");
+const password = document.querySelector("input[type=password]");
 
 signinBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const email = document.querySelector("input[type=email]").value;
-  const password = document.querySelector("input[type=password]").value;
-  localStorage.setItem("email", email);
-  localStorage.setItem("password", password);
+  localStorage.setItem("email", email.value);
+  localStorage.setItem("password", password.value);
+  window.location.href = "home.html"
 });
