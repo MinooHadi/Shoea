57279c6796fa8bcb30255cc703f0fffd14cbb651
Minoo <minoo.hadi2904@gmail.com>
2023-01-03 20:05:18 +0330

    feat: add slider for product image

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,3 +1,5 @@
+const BASE_URL = "https://638e0461aefc455fb2b27d63.mockapi.io/products";
+
 function goToHomePage() {
   window.location.href = "home.html";
 }
@@ -13,3 +15,89 @@ function plus() {
   const quantity = document.querySelector("#quantity input");
   quantity.value = Number(quantity.value) + 1;
 }
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const params = new URLSearchParams(window.location.search);
+  let productId = params.get("id");
+  let response = await axios.get(`${BASE_URL}/${productId}`);
+  let data = response.data;
+  console.log(data);
+
+  let imageUrls = data.imageUrl;
+  const slider = document.createElement("div");
+  slider.id = "slider";
+  for (let imageUrl of imageUrls) {
+    let sliderDiv = document.createElement("div");
+    slider.append(sliderDiv);
+
+    sliderDiv.addEventListener("click", () => {
+        const sliderDivs = document.querySelectorAll("#slider div");
+        sliderDivs.forEach(item => item.classList.remove("sliderDiv"));
+        document.querySelector("#image img").src = imageUrl;
+        sliderDiv.classList.add("sliderDiv");
+    })
+  }
+
+  const topDiv = document.querySelector("#top");
+  topDiv.innerHTML = `<div>
+    <i class="fa fa-long-arrow-left" onclick="goToHomePage()"></i>
+  </div>
+  <div id="image">
+    <img src="${imageUrls[0]}" alt="" />
+  </div>`;
+
+  topDiv.append(slider)
+
+  const bottomDiv = document.querySelector("#bottom");
+  bottomDiv.innerHTML = `<div id="name">
+  <h2>${data.name}</h2>
+  <i class="fa fa-heart-o"></i>
+</div>
+<div id="view">
+  <i class="fa fa-star-half-o"></i>
+  <p>${data.rate} (${data.reviews} reviews)</p>
+</div>
+<div class="hr"></div>
+<div id="description">
+  <h3>Description</h3>
+  <p>${data.description}</p>
+</div>
+<div id="size">
+  <h3>Size</h3>
+  <div id="numbers">
+    <div>38</div>
+    <div>39</div>
+    <div>40</div>
+    <div>41</div>
+    <div>42</div>
+  </div>
+</div>
+<div id="color">
+  <h3>color</h3>
+  <div id="colors">
+    <div style="background-color:${data.color[0]} ;"></div>
+    <div style="background-color:${data.color[1]} ;"></div>
+    <div style="background-color:${data.color[2]} ;"></div>
+    <div style="background-color:${data.color[3]} ;"></div>
+  </div>
+</div>
+<div id="quantity">
+  <h3>Quantity</h3>
+  <div>
+    <i class="fa fa-minus" onclick="minus()"></i>
+    <input type="number" value="1" readonly/>
+    <i class="fa fa-plus" onclick="plus()"></i>
+  </div>
+</div>
+<div class="hr"></div>
+<div id="price">
+  <div>
+    <p>Total price</p>
+    <h2>$${data.price}</h2>
+  </div>
+  <button>
+    <i class="fa fa-shopping-bag"></i>
+    <h4>Add to Cart</h4>
+  </button>
+</div>`;
+});
