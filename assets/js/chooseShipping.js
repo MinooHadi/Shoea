7ea5f36b8a4c33ec29d72fa8a9771fc46d5a2c78
Minoo <minoo.hadi2904@gmail.com>
2023-01-04 19:37:48 +0330

    feat: add shipping to localstorage

--- assets/js/chooseShipping.js
+++ assets/js/chooseShipping.js
@@ -1,3 +1,19 @@
 function goToCheckout() {
     window.location.href = "checkout.html"
 }
+
+function chooseShipping() {
+    const selectedShipping = document.querySelector("input:checked");
+    let data = selectedShipping.parentElement.parentElement;
+    let shipping = {
+        shippingIcon: data.querySelector("i").className,
+        shippingName: data.querySelector(".addressText h4").innerText,
+        shippingPrice: data.querySelector(".price").innerText,
+    }
+
+    shipping = JSON.stringify(shipping)
+    localStorage.setItem("shipping", shipping)
+
+    window.location.href = "checkout.html"
+
+}
