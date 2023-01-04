72d1bd3ab5019732c7c4051c410b3b4c0cb84a86
Minoo <minoo.hadi2904@gmail.com>
2023-01-04 18:08:04 +0330

    feat: get address

---
+++ assets/js/shippingAddress.js
@@ -0,0 +1,29 @@
+const BASE_URL = "https://638e0461aefc455fb2b27d63.mockapi.io/address";
+
+const getAddress = async () => {
+  const response = await axios.get(BASE_URL);
+  return response.data;
+};
+
+const createDOM = async () => {
+  const address = document.querySelector("#address");
+  const data = await getAddress();
+  for (let item of data) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<div class="location">
+        <i class="fa fa-map-marker"></i>
+      </div>
+      <div class="addressText">
+        <h4>${item.title}</h4>
+        <p>${item.address}</p>
+      </div>
+      <div>
+        <input type="checkbox" />
+      </div>`;
+
+      address.insertAdjacentElement("beforebegin", card)
+  }
+};
+
+window.addEventListener("DOMContentLoaded", createDOM);
